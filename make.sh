71adf65bcb0960c56a11969fed7239b131a9349c
Fengda HUANG <h@phodal.com>
2016-12-27 21:11:52 +0800

    create gh-pages branch

--- make.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ionic build android --release --prod
-rm ~/repractise/MoTreeApp/platforms/android/build/outputs/apk/growth.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/phodal.keystore ~/repractise/MoTreeApp/platforms/android/build/outputs/apk/android-release-unsigned.apk phodal
-~/android-sdk/build-tools/24.0.2/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk ~/repractise/MoTreeApp/platforms/android/build/outputs/apk/growth.apk
-cp ~/repractise/MoTreeApp/platforms/android/build/outputs/apk/growth.apk ~/Desktop/growth-tree.apk
