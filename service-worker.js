e655ab4b0cc7bf97e5dc6177a51cd774c69ac018
Fengda HUANG <h@phodal.com>
2016-12-28 08:45:15 +0800

    use relatvie link for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
 var dataCacheName = 'motree';
 var cacheName = 'motree-cache';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/lib/ionic/js/ionic.bundle.min.js',
-  '/lib/ionic/css/ionic.min.css',
-  '/lib/ionic/fonts/ionicons.woff',
-  '/lib/localforage.min.js',
-  '/lib/lodash.min.js',
-  '/lib/vivus.min.js',
-  '/js/app.js',
-  '/js/controllers.js',
-  '/js/data.js',
-  '/img/splash.svg',
-  '/img/icon.svg',
-  '/css/style.css'
+  'index.html',
+  'lib/ionic/js/ionic.bundle.min.js',
+  'lib/ionic/css/ionic.min.css',
+  'lib/ionic/fonts/ionicons.woff',
+  'lib/localforage.min.js',
+  'lib/lodash.min.js',
+  'lib/vivus.min.js',
+  'js/app.js',
+  'js/controllers.js',
+  'js/data.js',
+  'img/splash.svg',
+  'img/icon.svg',
+  'css/style.css'
 ];
 
 self.addEventListener('install', function(e) {
